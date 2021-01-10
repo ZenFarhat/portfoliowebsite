74a5b7656a18c3fd7ed5bf296fde1a3781d7489c
ZenFarhat <s3777800@student.rmit.edu.au>
2021-01-10 20:18:39 +1100

    PROJECT SHOWCASE WORKING!/GB

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-const travellyIcon = document.querySelector('.travelly')
-const box = document.querySelector(".box")
+// CONST'SES
+const boxAll = document.querySelectorAll(".box")
+
+const ejoyBtn = document.querySelector(".elena-joy-btn")
+const travellybtn = document.querySelector(".travelly-btn")
+const tdogBtn = document.querySelector(".tindog-btn")
 
 
-travellyIcon.addEventListener('click', () => {
-    box.classList.toggle("hidden")
+const ejoyBox = document.querySelector(".elena-joy-box")
+const travellyBox = document.querySelector(".travelly-box")
+const tdogBox = document.querySelector(".tindog-box")
+
+
+// FUNCTIONALITY
+addHidden()
+
+
+
+ejoyBtn.addEventListener('click', () => {
+    addHidden()
+    ejoyBox.classList.remove('hidden')
 })
+
+
+travellybtn.addEventListener('click', () => {
+    addHidden()
+    travellyBox.classList.remove('hidden')
+})
+
+tdogBtn.addEventListener('click', () => {
+    addHidden()
+    tdogBox.classList.remove('hidden')
+})
+
+
+function addHidden() {
+    boxAll.forEach(box => {
+        box.classList.add("hidden")
+    })
+}
+
+
+
